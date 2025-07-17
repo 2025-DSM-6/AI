f923d2d299de81f7a88e6775891cf3545dd4ded2
Park jaemin <hungryshark4292@gmail.com>
2025-07-17 15:33:33 +0900

    cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 미들웨어 등록
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(router)
